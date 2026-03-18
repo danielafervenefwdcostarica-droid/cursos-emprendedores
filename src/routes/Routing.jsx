eaadc01bc74c564b72fcbf173b09ca8365459cbb
quirosfwdcostarica-debug <quirosfwdcostarica@gmail.com>
2026-03-18 14:41:53 -0600

    commit

--- src/routes/Routing.jsx
+++ src/routes/Routing.jsx
@@ -1,13 +1,29 @@
-import { BrowserRouter as Router,Routes,Route } from "react-router-dom";
-import Inicio from "../pages/Home";
+import React from 'react';
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 
-function Routing() {
-    return(
-        <Router>
-            <Routes>
-                <Route path="/" element={<Inicio/>}/>
-            </Routes>
-        </Router>
-    )
-}
-export default Routing
+// Importamos todas tus vistas
+import LoginPage from '../components/LoginPage';
+import RegisterPage from '../components/RegisterPage'; 
+import ClientProfile from '../pages/ClientProfile';
+import AdminProfile from '../pages/AdminProfile';
+
+const AppRouter = () => {
+  return (
+    <BrowserRouter>
+      <Routes>
+        {/* Rutas Públicas */}
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/registro" element={<RegisterPage />} />
+        
+        {/* Rutas Privadas (Perfiles) */}
+        <Route path="/cliente" element={<ClientProfile />} />
+        <Route path="/admin" element={<AdminProfile />} />
+        
+        {/* Ruta por defecto: si entran a la raíz, los mandamos al login */}
+        <Route path="*" element={<Navigate to="/login" replace />} />
+      </Routes>
+    </BrowserRouter>
+  );
+};
+
+export default Routing;
