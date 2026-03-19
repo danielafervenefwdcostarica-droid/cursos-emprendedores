5e18ab2ff5544137e566659d79a66d5c4915b3fa
danielafervenefwdcostarica-droid <danielafervenefwdcostarica@gmail.com>
2026-03-19 10:51:15 -0600

    arreglos rutas

--- src/main.jsx
+++ src/main.jsx
@@ -1,15 +1,3 @@
-<<<<<<< HEAD
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App'
-
-
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App/>
-    </StrictMode>,
-)
-=======
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 // Importamos el nuevo Routing
@@ -22,4 +10,3 @@ ReactDOM.createRoot(document.getElementById('root')).render(
     <Routing />
   </React.StrictMode>,
 );
->>>>>>> 904a23ff299a345a013f93862f2ca81b228f3f70
--- src/routes/Routing.jsx
+++ src/routes/Routing.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 
-<<<<<<< HEAD
 // Importamos todas tus vistas
 import LoginPage from '../pages/LoginPage';
 import Registro from '../pages/Registro'; 
@@ -11,22 +10,20 @@ import CursosIdi from '../pages/CursosIdi';
 import CursosTec from '../pages/CursosTec';
 import CursosArte from '../pages/CursosArte';
 import CursosBe from '../pages/CursosBe';
-=======
-import LoginPage from '../components/LoginPage';
-import RegisterPage from '../components/RegisterPage'; 
-import ClientProfile from '../pages/ClientProfile';
-import AdminProfile from '../pages/AdminProfile';
+import Inicio from '../pages/Home';
+import OpportunitiesHome from '../pages/OpportunitiesHome';
 
->>>>>>> 904a23ff299a345a013f93862f2ca81b228f3f70
 const Routing = () => {
   return (
     <BrowserRouter>
       <Routes>
-<<<<<<< HEAD
         {/* Rutas Públicas */}
         <Route path="/login" element={<LoginPage />} />
         <Route path="/registro" element={<Registro />} />
-        
+        <Route path="/oportunidades" element={<OpportunitiesHome />} />
+        <Route path="/inicio" element={<Inicio />} />
+
+
         {/* Rutas Privadas (Perfiles) */}
         <Route path="/cliente" element={<ClientProfile />} />
         <Route path="/admin" element={<AdminProfile />} />
@@ -40,15 +37,6 @@ const Routing = () => {
         <Route patch="/CursosArtesania" element={<CursosArte/>}/>
         <Route path="/Cursos Belleza y estética" element={<CursosBe/>}/>
 
-=======
-        <Route path="/login" element={<LoginPage />} />
-        <Route path="/registro" element={<RegisterPage />} />
-        
-        <Route path="/cliente" element={<ClientProfile />} />
-        <Route path="/admin" element={<AdminProfile />} />
-        
-        <Route path="*" element={<Navigate to="/login" replace />} />
->>>>>>> 904a23ff299a345a013f93862f2ca81b228f3f70
       </Routes>
     </BrowserRouter>
   );
